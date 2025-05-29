acdbc3cd8d15a47fd225de6f990a1f087a2179cc
Roar-Morkore-Hansen <psd254@alumni.ku.dk>
2025-05-29 01:51:28 +0100

    feat: abbility to select verses and play audio from selected verses. Also logging feature

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
-var x = document.getElementById("audio-vers_1"); 
+
+print_log = true;
+
+function log(string) {
+    if (print_log == true) {
+        console.log(string);
+    }
+}
+
+// Deselecet prevoius and select new
+function selectVerse(id) {
+    log("[selectVerse] Selected id: " + id)
+
+    var verse_id_new = id;
+    var verse_id_prev = document.querySelector(".selected").id;
+    document.getElementById(verse_id_prev).classList.toggle("selected");
+    document.getElementById(verse_id_new).classList.toggle("selected");
+
+};
+
+
+//Select verse when clicked on.
+document.querySelectorAll(".verse").forEach(item => {
+    item.addEventListener("click", event => {
+        log(event.target)
+        selectVerse(event.target.id) 
+    })
+    log("[eventlistener] addEventListener to item id:" + item.id)
+});
+
 
 document.onkeydown = function checkKey(event) {
     if (event.key == "Enter") {
-        x.play()
+        document.querySelector(".selected").parentElement.firstElementChild.play()
     }
 };
+
+// DRIVER CODE
+
+
